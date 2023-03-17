bead1b6ff69458b8dd2d6a09a98a012d75b1ac9f
Akaban3dit <newakbn13@gmail.com>
2023-03-17 09:50:15 -0500

    Se agrego al nuevo

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import DashboardView from '../views/DashboardView.vue'
 import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
@@ -7,8 +7,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Login',
+      component: LoginView
     },
     {
       path: '/about',
@@ -16,10 +16,11 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/login',
-      name: 'login',
-      component: LoginView
+      path: '/Dashboard',
+      name: 'Dashboard',
+      component: DashboardView
     }
+    
   ]
 })
 
